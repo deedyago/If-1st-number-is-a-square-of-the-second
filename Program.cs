aece4d2ef80dc8277f26a63ff88044a4d096ea79
deedyago <deedyagobin@gmail.com>
2022-07-21 21:59:34 +0300

    Finall commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿int number = 0;
 int number2 = 0;
-int number3 = 0;
 
 Console.Write("Введите первое число: ");
 number = int.Parse(Console.ReadLine());
@@ -8,13 +7,13 @@ number = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 number2 = int.Parse(Console.ReadLine());
 
-if(number > number2)
+if (number < number2)
 {   
-        Console.Write("Не является");
-    }
-else 
-    int number3 = number*number;
-    if(number3 == number2)
+    Console.Write("Не является");
+}
+else{
+    int number3 = number2*number2;
+    if(number3 == number)
     {  
         Console.Write("Является");
     }
@@ -23,3 +22,4 @@ else
         Console.Write("Не является");
     }
 }
+    
